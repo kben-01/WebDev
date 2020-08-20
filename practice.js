49f3be599eb4c9f2d0be36d883515c1f82be20a7
kobe benson <kobebenson15@gmail.com>
2020-08-19 20:41:17 -0600

    added practice.js

---
+++ practice.js
@@ -0,0 +1,12 @@
+var person = {
+    firstName: "Kobe",
+    lastName: "Benson",
+    age: 19,
+
+};
+
+const obj = {key: 'value'}
+const json = JSON.stringify(obj)
+console.log(json)
+
+  console.log("Hello World")
